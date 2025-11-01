56058126bbafdf39f8fae28d615489edaf964782
BukreevYas <bukreev.yas@edu.spbstu.ru>
2025-11-01 12:33:28 +0300

    Added template for work with matrix.

--- main.cpp
+++ main.cpp
@@ -1,2 +1,24 @@
+#include <iostream>
+
+int** makeMatrix(int rows, int cols);
+void destroyMatrix(int** matrix);
+void outputMatrix(const int* const* matrix):
+
 int main()
-{}
+{
+  int rows = 0, cols = 0;
+  std::cin >> rows >> cols;
+
+  if (!std::cin)
+  {
+    return 1;
+  }
+
+  int** matrix = nullptr;
+
+  matrix = makeMatrix(rows, cols);
+
+  outputMatrix(matrix);
+
+  destroyMatrix(matrix);
+}
